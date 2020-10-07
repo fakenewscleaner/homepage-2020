42f2898fe8bd943f0b2f7340f6959381d6830a4c
Chang Shu-huai <junsuwhy@gmail.com>
2020-10-07 20:22:04 +0800

    Update formatting.

--- js/main.js
+++ js/main.js
@@ -23,65 +23,15 @@ $(document).ready(function(){
     });
      
     $('select').niceSelect();
-    $('.img-pop-up').magnificPopup({
-        type: 'image',
-        gallery:{
-        enabled:true
-        }
-    });
 
-
-    $('.active-exprience-carousel').owlCarousel({
-        center: true,
-        items:1,
-        loop:true
-    })
-    $('.next-trigger').click(function() {
-        $(".active-exprience-carousel").trigger('next.owl.carousel');
-    })
-        // Go to the previous item
-    $('.prev-trigger').click(function() {
-        $(".active-exprience-carousel").trigger('prev.owl.carousel');
-    });
-    $('.play-btn').magnificPopup({
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-    $(document).ready(function() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    });      
-    // -------   Mail Send ajax
-
-     $(document).ready(function() {
-        var form = $('#myForm'); // contact form
-        var submit = $('.submit-btn'); // submit button
-        var alert = $('.alert-msg'); // alert div for show alert message
-
-        // form submit event
-        form.on('submit', function(e) {
-            e.preventDefault(); // prevent default form submit
-
-            $.ajax({
-                url: 'mail.php', // form action url
-                type: 'POST', // form submit method get/post
-                dataType: 'html', // request type html/json/xml
-                data: form.serialize(), // serialize form data
-                beforeSend: function() {
-                    alert.fadeOut();
-                    submit.html('Sending....'); // change submit button text
-                },
-                success: function(data) {
-                    alert.html(data).fadeIn(); // fade in response data
-                    form.trigger('reset'); // reset form
-                    submit.attr("style", "display: none !important");; // reset submit button text
-                },
-                error: function(e) {
-                    console.log(e)
-                }
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
             });
         });
     });
+    
  });
